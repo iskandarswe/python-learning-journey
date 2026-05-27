b779438feea87235b06f516387db9308543db8ce
kali <kali@kali.kali>
2026-05-27 18:38:08 +0500

    add py files

---
+++ 1-dars.py
@@ -0,0 +1,8 @@
+# 03-dars: PRINT() funksiyasi
+# Sana: 05/11/2025
+# Mualif:Anvar
+
+
+print("So'zlarin qanchalik chiroyli bo'lmasin,\nAmalingdan so'ralasan")
+
+print("To'qqizning kvadrati", 9**2, "ga teng")
